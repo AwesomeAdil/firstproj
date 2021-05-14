eae5f2adbed762c480ae9fdc7de6f8998ea1e04a
Adil Bhatti <adilbhatti3011@gmail.com>
2021-05-14 10:30:59 -0500

    added static files

--- firstproj/firstproj/settings.py
+++ firstproj/firstproj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -114,6 +114,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Static files (CSS, JavaScript, Images)
